59a4049319a4f820c2b787828c3fdd2801ed22d7
kamily <kamily.kammer@escola.pr.gov.br>
2025-07-02 19:39:31 +0000

    Aula 09 parte 2

--- src/main/java/com/mycompany/Aula09Objetos.java
+++ src/main/java/com/mycompany/Aula09Objetos.java
@@ -2,22 +2,61 @@ package com.mycompany;
 
 public class Aula09Objetos {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      /*
-      * Em java, toda classe ou tipo abstrato é um objeto, pois possuem caracteristicas
-      * e funcionalidades.
-      *
-      * Eles herdam funções da classe mãe java.lang.Object, é correto  afirmar que java.lang.Object é uma super classe de qualquer outra classe ou tipo abstrato.
-      */ 
-      Integer numero = 10; //Tipo abstrato
-      int numero2 = 10; // Tipo primitivo
+    /*
+     * Em java, toda classe ou tipo abstrato é um objeto, pois possuem
+     * caracteristicas
+     * e funcionalidades.
+     *
+     * Eles herdam funções da classe mãe java.lang.Object, é correto afirmar que
+     * java.lang.Object é uma super classe de qualquer outra classe ou tipo
+     * abstrato.
+     */
+    Integer numero = 10; // Tipo abstrato
+    int numero2 = 10; // Tipo primitivo
 
-      // Retorna se a variavel/calsse é uma instancia de outra classe
-      System.out.println(numero instanceof Object);
+    // Retorna se a variavel/calsse é uma instancia de outra classe
+    System.out.println(numero instanceof Object);
 
-      // Método compareTo pertence ao tipo abstrato Integer
-      numero.compareTo(numero2);
-        
-    }    
+    // Método compareTo pertence ao tipo abstrato Integer
+    numero.compareTo(numero2);
+
+    // Instância uma classe
+    Carro carro = new Carro("Celta", "Chevrolet");
+    carro.andar(60);
+
+  }
+
+  /*
+   * Criando uma classe dentro de outra classe
+   * é necessario utilizar a instrução static
+   * entre public e class
+   */
+  public static class Carro {
+    // Atributo da classe
+    String modelo;
+    String marca;
+    int ano;
+    int quilometragem = 0;
+
+    /**
+     * Método construtor
+     * 
+     * ele é chamado quando criamos uma nova instância da classe.
+     */
+
+    public Carro(String modelo, String marca) {
+      this.modelo = modelo;
+      this.marca = marca;
+    }
+
+    // Ações da classe
+    void andar(int km) {
+      this.quilometragem += km;
+      System.out.println("O carro está percorrendo " + km + "km");
+    }
+
+  }
+
 }
