e149dc035e984031616a1ba53a3ea2d8c1a79136
kamily <kamily.kammer@escola.pr.gov.br>
2025-08-27 19:38:45 +0000

    ex 05

---
+++ src/main/java/com/mycompany/poo/ex/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.poo.ex;
+
+public class App {
+    public static void main(String[] args) {
+        Time time = new Time();
+
+        time.setNome("Flamengo");
+        time.setAbreviacao("FLA");
+        time.setTorcida(55000000);
+        time.setEstado("Rio de Janeiro");
+        time.setCidade("Rio de Janeiro");
+
+    }
+}
---
+++ src/main/java/com/mycompany/poo/ex/Calcado.java
@@ -0,0 +1,41 @@
+package com.mycompany.poo.ex;
+
+public class Calcado {
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int tamanho;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+}
